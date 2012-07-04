486f49b2a810c74a828753a167f2d6dc7ab1bccd
Josh Mandel <Joshua.Mandel@childrens.harvard.edu>
2012-07-03 22:28:48 -0700

    Broke out oid library, processors

--- ccd.py
+++ ccd.py
@@ -1,4 +1,6 @@
 import datetime, copy, collections, hashlib
+from oids import OID_Library
+from processors import *
 
 BASE_URI = "http://smart-catcher/"
 
@@ -7,170 +9,6 @@ ns = {
     "xsi": "http://www.w3.org/2001/XMLSchema-instance"
 }
 
-class OID_Library(object):
-    registered = []
-
-    @classmethod
-    def register(cls, oidclass):
-        cls.registered.append(oidclass)
-
-    @classmethod
-    def by_oid(cls, oid):
-        return filter(lambda x: x.oid==oid, cls.registered)[0]
-
-    @classmethod
-    def by_name(cls, name):
-        return filter(lambda x: x.name==name, cls.registered)[0]
-
-@OID_Library.register
-class CDC_Race():
-    oid = "2.16.840.1.113883.6.238"
-    name = "CDC Race"
-    uri = "http://phinvads.cdc.gov/vads/ViewCodeSystemConcept.action?oid=2.16.840.1.113883.6.238&code="
-
-    
-@OID_Library.register
-class LOINC():
-    oid = "2.16.840.1.113883.6.1"
-    name = "LOINC"
-    uri =  "http://purl.bioontology.org/ontology/LNC/"
-
-@OID_Library.register
-class RXNORM():
-    oid = "2.16.840.1.113883.6.88"
-    name = "RXNORM"
-    uri =  "http://purl.bioontology.org/ontology/RXNORM/"
-
-
-@OID_Library.register
-class SNOMED():
-    oid = "2.16.840.1.113883.6.96"
-    name = "SNOMED CT"
-    uri =  "http://purl.bioontology.org/ontology/SNOMEDCT/"
-
-@OID_Library.register
-class GoodHealth():
-    oid = "2.16.840.1.113883.19"
-    name = "Good Health Clinic"
-    uri =  "http://hl7.org/goodhealth/"
-
-@OID_Library.register
-class ReligiousAffiliation():
-    oid = "2.16.840.1.113883.1.11.19185" 
-    name = "HL7 Religion"
-    uri =  "http://hl7.org/codes/ReligiousAffiliation#"
-
-@OID_Library.register
-class LanguageAbility():
-    oid = "2.16.840.1.113883.5.60" 
-    name = "HL7 LanguageAbility"
-    uri =  "http://hl7.org/codes/LanguageAbility#"
-
-@OID_Library.register
-class MaritalStatus():
-    oid = "2.16.840.1.113883.5.2" 
-    name = "HL7 Marital Status"
-    uri =  "http://hl7.org/codes/MaritalStatus#"
-
-@OID_Library.register
-class ResultInterpretation():
-    oid = "2.16.840.1.113883.5.83"
-    name = "HL7 Result Interpretation"
-    uri =  "http://hl7.org/codes/ResultInterpretation#"
-    table = {
-    'B':'better',
-    'D':'decreased',
-    'U':'increased',
-    'W':'worse',
-    '<':'low off scale',
-    '>':'high off scale',
-    'A':'Abnormal',
-    'AA':'abnormal alert',
-    'H':'High',
-    'HH':'high alert',
-    'L':'Low',
-    'LL':'low alert',
-    'N':'Normal',
-    'I':'intermediate',
-    'MS':'moderately susceptible',
-    'R':'resistent',
-    'S':'susceptible',
-    'VS':'very susceptible',
-    'EX':'outside threshold',
-    'HX':'above high threshold',
-    'LX':'below low threshold',
-    }
-
-@OID_Library.register
-class PersonalRelationship():
-    oid = "2.16.840.1.113883.5.111"
-    name = "HL7 Role"
-    uri =  "http://hl7.org/codes/PersonalRelationship#"
-
-@OID_Library.register
-class AddressUse():
-    oid = "2.16.840.1.113883.5.1119"
-    name = "HL7 Address"
-    uri =  "http://hl7.org/codes/Address#"
-    table = {
-    'BAD': 'bad address',
-    'CONF': 'confidential',
-    'DIR': 'direct',
-    'H': 'home address',
-    'HP': 'primary home',
-    'HV': 'vacation home',
-    'PHYS': 'physical visit address',
-    'PST': 'postal address',
-    'PUB': 'public',
-    'TMP': 'temporary',
-    'WP': 'work place'
-    }
-
-@OID_Library.register
-class EntityNameUse():
-    oid = "2.16.840.1.113883.5.45"
-    name = "HL7 EntityName"
-    uri =  "http://hl7.org/codes/EntityName#"
-    table = {
-    'A': 'Artist/Stage',
-    'ABC': 'Alphabetic',
-    'ASGN': 'Assigned',
-    'C': 'License',
-    'I': 'Indigenous/Tribal',
-    'IDE': 'Ideographic',
-    'L': 'Legal',
-    'P': 'Pseudonym',
-    'PHON': 'Phonetic',
-    'R': 'Religious',
-    'SNDX': 'Soundex',
-    'SRCH': 'Search',
-    'SYL': 'Syllabic'
-    }
-
-@OID_Library.register
-class AdministrativeGender():
-    oid = "2.16.840.1.113883.5.1"
-    name = "HL7 AdministrativeGender"
-    uri =  "http://hl7.org/codes/AdministrativeGender#"
-    table = {
-    'F': 'Female',
-    'M': 'Male',
-    'UN': 'Undifferentiated'
-    }
-
-"""
-MaritalStatus = {
-'A ': 'Annulled',
-'D ': 'Divorced',
-'I ': 'Interlocutory',
-'L ': 'Legally Separated',
-'M ': 'Married',
-'P ': 'Polygamous',
-'S ': 'Never Married',
-'T ': 'Domestic partner',
-'W ': 'Widowed '
-}
-"""
 
 def xpath(doc, p):
     r= doc.xpath(p, namespaces=ns)
@@ -213,50 +51,6 @@ class SubStructure:
                 j = j[0]
         if j:
             caller.subs[self.subpath] = j
-        """ Todo: bring this back at json generation time
-        if j:
-            components = self.jsonpath.split(".")
-            path = json
-            for p in components[:-1]:
-                if p not in path:
-                    path[p] = {}
-                path = path[p]
-                    
-            path[components[-1]] = j
-        """
-
-def HL7Timestamp(t, parent):
-    da = []
-    da.append(len(t)>=4 and int(t[0:4]) or 1)
-    da.append(len(t)>=6 and int(t[4:6]) or 1)
-    da.append(len(t)>=8 and int(t[6:8]) or 0)
-    da.append(len(t)>=10 and int(t[8:10]) or 0)
-    da.append(len(t)>=12 and int(t[10:12]) or 0)
-    da.append(len(t)>=14 and int(t[12:14]) or 0)
-    return datetime.datetime(*da)
-
-def HL7TimestampResolution(t, parent):
-    if len(t)==4:
-        return 'year';
-    if len(t)==6:
-        return 'month';
-    if len(t)==8:
-        return 'day';
-    if len(t)==10:
-        return 'hour';
-    if len(t)==12:
-        return 'minute';
-    if len(t)==14:
-        return 'second';
-    else:
-        assert len(t)>14, "unexpected timestamp length %s:%s"%(t,len(t))
-        return 'subsecond'
-
-def StringMap(m):
-    def transform(x):
-        return m[x]
-    return transform
-
 class Importer(object):
     fields = []
     js_template = {}
@@ -579,8 +373,10 @@ class ConsolidatedCDA(Importer):
             ("vitals", "0..1", VitalSignsSection.xpath, VitalSignsSection),
             ("results", "0..1", ResultsSection.xpath, ResultsSection),
            ]
+
     def __init__(self, doc):
         super(ConsolidatedCDA, self).__init__(doc, parent=None)
-        self.record_id = self.subs['demographics'].mrn
 
+    def precleanup(self):
+        self.record_id = self.subs['demographics'].mrn
         super(ConsolidatedCDA, self).postprocess()
