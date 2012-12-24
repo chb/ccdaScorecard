908d044bb36a634636988c152661b594743a24e6
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2012-12-24 14:09:20 -0800

    Showing section-wise scorecard

--- rubrics/labcodes.js
+++ rubrics/labcodes.js
@@ -11,19 +11,23 @@ rubric.prototype.report = function(done){
   var numerator = codes.inSet.length;
   var denominator = codes.notInSet.length + numerator;
 
-  var report = common.report(rubric, numerator, denominator, {misses: codes.notInSet});
+  var report = common.report(
+    rubric, 
+    numerator, 
+    denominator, 
+    {misses: codes.notInSet}
+  );
 
   done(null, report);
 };
 
 var labCodeXpath = "( \
-    //h:templateId[@root='2.16.840.1.113883.10.20.22.2.3']/.. | \
-    //h:templateId[@root='2.16.840.1.113883.10.20.22.2.3.1']/.. \
-  )\
-  //h:templateId[@root='2.16.840.1.113883.10.20.22.4.2']/../\
-  h:code";
+//h:templateId[@root='2.16.840.1.113883.10.20.22.2.3']/.. | \
+//h:templateId[@root='2.16.840.1.113883.10.20.22.2.3.1']/.. \
+)\
+//h:templateId[@root='2.16.840.1.113883.10.20.22.4.2']/../\
+h:code";
 
-// internally used value set OID surrogate
-// because there's no official OID for the 
-// top-2k extract
+// internal Value Set OID surrogate, since
+// there's no official OID for this top-2k extract
 var valueSetOid = "LoincTop2000CodesUSAValueSet";
